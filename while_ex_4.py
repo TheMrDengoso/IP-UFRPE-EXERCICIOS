2f64efba98fd0508f71333e3dce84e683c9fdbac
TheMrDengoso <43834934+TheMrDengoso@users.noreply.github.com>
2019-04-07 21:57:10 -0300

    Update while_ex_4.py

--- while_ex_4.py
+++ while_ex_4.py
@@ -1,10 +1,12 @@
 g=0
-n=0
+n=1
+print("2\n3\n5")
 while True:
     n+=1
     if(n%2!=0):
-        g+=1
-        print(n)
-        if(g==15):
-            break
-        
+        if(n%3!=0):
+            if(n%5!=0):
+                g+=1
+                print(n)
+                if(g==15):
+                    break
