d01e30545d6de661a5bace877fe5e92e30639058
TheMrDengoso <43834934+TheMrDengoso@users.noreply.github.com>
2019-04-16 16:51:14 -0300

    Create lista4_ex_4.py

---
+++ lista4_ex_4.py
@@ -0,0 +1,8 @@
+mes = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+data = str(input("Digite sua data de nascimento: "))
+data = data.split("/")
+mes_num = int(data[1])
+for i in range(12):
+  if(mes_num==i+1):
+    print("{} de {} de {}".format(data[0],mes[i],data[2]))
+  
